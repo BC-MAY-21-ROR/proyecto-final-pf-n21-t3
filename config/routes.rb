ede33662569be3f418e91195c4862e65a847ef74
IrenehMDev <ireneh.dev_03@icloud.com>
2022-01-26 03:34:22 -0600

    Devise routes and log-out base created
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: DerekAyala <derek.ayala12345@hotmail.com>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  #devise_for :users
-  root to: "users#index"
- 
+  devise_for :users
   devise_scope :user do
-    get '/signout', to: 'devise/sessions#destroy', as: :signout
+    get 'users/sign_out' => 'devise/sessions#destroy'
+    get 'users/log_in'   => 'devise/sessions#new'
+    get 'users/log_out'  => 'devise/sessions#destroy'
+    get 'users/sign_up'  => 'devise/registrations#new'
+    get 'users/edit_profile' => 'devise/registrations#edit'
+ 
+  #devise_for :users
+  #devise_scope :user do
+   # root to: 'users#index'
+   # get '/users/sign_out' => 'devise/sessions#destroy'
   end
 end
