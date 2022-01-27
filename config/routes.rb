2d0210942f3664c2f9380b79e3bb98eb1b04df5c
Villaedi <ediberto.sanchez.villalobos@gmail.com>
2022-01-26 19:44:35 -0600

    Delete comments of routes.rb
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: DerekAyala <derek.ayala12345@hotmail.com>
    Co-authored-by: IrenehMDev <ireneh.dev_03@icloud.com>

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,5 @@ Rails.application.routes.draw do
     get 'users/log_out'  => 'devise/sessions#destroy'
     get 'users/sign_up'  => 'devise/registrations#new'
     get 'users/edit_profile' => 'devise/registrations#edit'
- 
-  #devise_for :users
-  #devise_scope :user do
-   # root to: 'users#index'
-   # get '/users/sign_out' => 'devise/sessions#destroy'
   end
 end
