b1c874caaec9acab24973e728d6638534d7b10c4
IrenehMDev <ireneh.dev_03@icloud.com>
2022-02-02 19:13:09 -0600

    Devise's controllers deleted, last_name deleted
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: DerekAyala <derek.ayala12345@hotmail.com>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :last_name, :email, :password, :birthday, :gender) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :last_name, :email, :password, :current_password, :birthday, :gender) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :birthday, :gender) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :birthday, :gender) }
   end
 end
