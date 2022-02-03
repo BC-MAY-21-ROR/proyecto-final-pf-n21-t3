a58563072a48f10dcef5db06c3cfd776ae8aef69
IrenehMDev <ireneh.dev_03@icloud.com>
2022-02-02 19:19:46 -0600

    Renamed pages-controller to feeds-controller
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: DerekAyala <derek.ayala12345@hotmail.com>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>

---
+++ app/controllers/feeds_controller.rb
@@ -0,0 +1,4 @@
+class FeedsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#feed'
+  root to: 'feeds#index'
   devise_for :users
-  
+
   devise_scope :user do
     get 'users/sign_out' => 'devise/sessions#destroy'
     get 'users/log_in'   => 'devise/sessions#new'
