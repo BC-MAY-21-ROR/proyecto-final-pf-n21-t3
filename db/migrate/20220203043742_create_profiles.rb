bb2f58f2dc1ae393529f04def64d4a402c483eb1
DerekAyala <derek.ayala12345@hotmail.com>
2022-02-02 23:12:10 -0600

    Database Profile Created
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>
    Co-authored-by: IrenehMDev <ireneh.dev_03@icloud.com>

---
+++ db/migrate/20220203043742_create_profiles.rb
@@ -0,0 +1,13 @@
+class CreateProfiles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :profiles do |t|
+      t.string :phone_num
+      t.string :city
+      t.string :state
+      t.string :country
+      t.text :description
+      t.belongs_to :user
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_025004) do
+ActiveRecord::Schema.define(version: 2022_02_03_043742) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "phone_num"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
