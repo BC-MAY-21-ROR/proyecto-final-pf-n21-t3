0b4d60c5a11dfaefa42d37b560b97394a1416504
Villaedi <ediberto.sanchez.villalobos@gmail.com>
2022-02-08 17:46:09 -0600

    Fixed schema last_name delete
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: DerekAyala <derek.ayala12345@hotmail.com>
    Co-authored-by: IrenehMDev <ireneh.dev_03@icloud.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_185728) do
+ActiveRecord::Schema.define(version: 2022_02_08_234338) do
 
   create_table "profiles", force: :cascade do |t|
     t.string "phone_num"
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 2022_02_07_185728) do
     t.string "name"
     t.date "birthday"
     t.string "gender"
-    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
