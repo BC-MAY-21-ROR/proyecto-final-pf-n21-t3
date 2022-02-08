87f47e053e6208b62fa724e17ee12cbf645ee2a0
DerekAyala <derek.ayala12345@hotmail.com>
2022-02-08 17:15:54 -0600

    Create Profile view and accion complete - also validates empty field
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>
    Co-authored-by: IrenehMDev <ireneh.dev_03@icloud.com>

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,16 +4,25 @@ class ProfilesController < ApplicationController
 
   def index
     @currentUser = current_user
-    @profile = Profile.find_by(id: current_user.id)
+    @profile = Profile.find_by(user_id: current_user.id)
   end
 
   def new
-  end
-
-  def edit
+    @profile = Profile.new
   end
 
   def create
+    hash = params.require(:profile).permit(:description, :city, :state, :country, :phone_num, :visible)
+    hash[:user_id] = current_user.id
+    @profile = Profile.new(hash)
+    if @profile.save
+      redirect_to profiles_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
   end
 
   def update
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,4 @@
 class Profile < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
+  validates :phone_num, :city, :state, :country, :description, presence: true
 end
