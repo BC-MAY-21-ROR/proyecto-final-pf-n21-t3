9a51511ec42b13fc35028b030f5188d9ae991794
DerekAyala <derek.ayala12345@hotmail.com>
2022-02-15 00:11:01 -0600

    move dependent: destroy to user model
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>
    Co-authored-by: IrenehMDev <ireneh.dev_03@icloud.com>

--- app/models/social_network.rb
+++ app/models/social_network.rb
@@ -1,4 +1,4 @@
 class SocialNetwork < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   validates :url, format: URI::DEFAULT_PARSER.make_regexp(['http', 'https'])
 end
