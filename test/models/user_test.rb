e36b718b1de2f10a02be637cc28be311e0a4906b
DerekAyala <derek.ayala12345@hotmail.com>
2022-03-08 22:21:06 -0600

    add a setup funtion to user test
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>
    Co-authored-by: IrenehMDev <ireneh.dev_03@icloud.com>

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
 
-  test 'User should be valid' do
-    user = User.new(
+  def setup
+    @user = User.new(
       email:"dayala0@hotmail.com",
       name:"Derek",
       birthday:"01-01-2001",
@@ -11,43 +11,32 @@ class UserTest < ActiveSupport::TestCase
       password:"dere2023",
       password_confirmation:"dere2023"
     )
-    assert user.valid?
   end
 
-  test 'User should not be valid because all fields are empty' do
-    user = User.new(
-      email:"",
-      name:"",
-      birthday:"",
-      gender:"",
-      password:"",
-      password_confirmation:""
-    )
-    assert_not user.valid?
+  test 'User should be valid' do
+    assert @user.valid?
+  end
+
+  test 'User should not be valid because a field is empty' do
+    @user.email = ""
+    assert_not @user.valid?
   end
 
   test 'User should not be valid because passwords are diferent' do
-    user = User.new(
-      email:"dayala0@hotmail.com",
-      name:"Derek",
-      birthday:"01-01-2001",
-      gender:"male",
-      password:"dere2023",
-      password_confirmation:"dere202"
-    )
-    assert_not user.valid?
+    @user.password = "dere2023"
+    @user.password_confirmation = "dere202"
+    assert_not @user.valid?
   end
 
   test 'User should not be valid because email have to be unique' do
-    User.new(
+    user2 = User.new(
       email:"dayala0@hotmail.com",
       name:"Derek",
       birthday:"01-01-2001",
       gender:"male",
       password:"dere2",
       password_confirmation:"dere2"
-    ).save
-    user2 = User.new(email:"dayala0@hotmail.com")
+    )
     assert_not user2.valid?
   end
 end
