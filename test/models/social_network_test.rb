450a32c21686ea3d7201af9cc73588d6aacbe7da
DerekAyala <derek.ayala12345@hotmail.com>
2022-03-08 22:41:30 -0600

    Unit test for social network model
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>
    Co-authored-by: IrenehMDev <ireneh.dev_03@icloud.com>

---
+++ test/models/social_network_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class SocialNetworkTest < ActiveSupport::TestCase
+  def setup
+    @user = User.create(
+      email:"dayala0@hotmail.com", name:"Derek", birthday:"01-01-2001",
+      gender:"male", password:"dere2023", password_confirmation:"dere2023"
+    )
+
+    @network = SocialNetwork.new(
+      url:"https://www.facebook.com/derek.ayala.96/", network: "Facebook", user_id: @user.id
+    )
+  end
+
+  test 'Social Network should be valid' do
+    assert @network.valid?
+  end
+
+  test 'Social Network should not be valid because is empty' do
+    @network.url = ""
+    assert_not @network.valid?
+  end
+
+  test 'Social Network should not be valid because is not a valid url' do
+    @network.url = "Facebook/perfil"
+    assert_not @network.valid?
+  end
+end
