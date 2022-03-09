a136719b2fc5177730befd70d2c0c16f301ce96d
DerekAyala <derek.ayala12345@hotmail.com>
2022-03-08 21:12:36 -0600

    unit test for user model
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>
    Co-authored-by: IrenehMDev <ireneh.dev_03@icloud.com>

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -13,4 +13,41 @@ class UserTest < ActiveSupport::TestCase
     )
     assert @user.valid?
   end
+
+  test 'User should not be valid because all fields are empty' do
+    @user = User.new(
+      email:"",
+      name:"",
+      birthday:"",
+      gender:"",
+      password:"",
+      password_confirmation:""
+    )
+    assert_not @user.valid?
+  end
+
+  test 'User should not be valid because passwords are diferent' do
+    @user = User.new(
+      email:"dayala0@hotmail.com",
+      name:"Derek",
+      birthday:"01-01-2001",
+      gender:"male",
+      password:"dere2023",
+      password_confirmation:"dere202"
+    )
+    assert_not @user.valid?
+  end
+
+  test 'User should not be valid because email have to be unique' do
+    @user1 = User.new(
+      email:"dayala0@hotmail.com",
+      name:"Derek",
+      birthday:"01-01-2001",
+      gender:"male",
+      password:"dere2",
+      password_confirmation:"dere2"
+    ).save
+    @user2 = User.new(email:"dayala0@hotmail.com")
+    assert_not @user2.valid?
+  end
 end
