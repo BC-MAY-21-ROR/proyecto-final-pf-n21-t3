c15d9368410c3be7eb447bf1c4c89ef828c20dbb
DerekAyala <derek.ayala12345@hotmail.com>
2022-03-08 20:55:58 -0600

    change to minitest and run the 1st test
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>
    Co-authored-by: IrenehMDev <ireneh.dev_03@icloud.com>

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'User should be valid' do
+    @user = User.new(
+      email:"dayala0@hotmail.com",
+      name:"Derek",
+      birthday:"01-01-2001",
+      gender:"male",
+      password:"dere2023",
+      password_confirmation:"dere2023"
+    )
+    assert @user.valid?
+  end
 end
